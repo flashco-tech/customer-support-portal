15ae732b93d45b85dbe25c9cfd9aa4fa4f7ff159
puruagarwal <puruagarwal@flash.tech>
2023-08-14 16:14:22 +0000

    fixes in referral page

--- pages/Customer Referral Details/jsobjects/ReferralDataProcessor/ReferralDataProcessor.js
+++ pages/Customer Referral Details/jsobjects/ReferralDataProcessor/ReferralDataProcessor.js
@@ -1,16 +1,5 @@
 export default {
 	myVar1: [],
-	myVar2: {},
-	getReferralDataByInput () {
-		var uuid = InputCustomerId.inputText;
-		var flashId = InputFlashId.inputText;
-		
-		if(uuid.length > 0)
-			GetCustomerReferralsByUUID.run();
-		else if(flashId.length)
-			GetCustomerReferralsByFlashId.run();
-		
-	},
 	getReferralData () {
 		var uuid = InputCustomerId.inputText;
 		var flashId = InputFlashId.inputText;
@@ -19,6 +8,7 @@ export default {
 			return GetCustomerReferralsByUUID.data;
 		else if(flashId.length)
 			return GetCustomerReferralsByFlashId.data;
+		else return [];
 		
 	},
 	async myFun2 () {
