4eeeda1a699eae941966479d5a107561e52503ca
Aniket Sinha <aniket@flash.tech>
2023-09-27 08:52:28 +0000

    splits referee details

--- pages/Customer Referral Details/jsobjects/ReferralDataProcessor/ReferralDataProcessor.js
+++ pages/Customer Referral Details/jsobjects/ReferralDataProcessor/ReferralDataProcessor.js
@@ -1,39 +1,32 @@
 export default {
 	myVar1: [],
 	getReferralDataByInput () {
-		var uuid = InputCustomerId.inputText;
-		var flashId = InputFlashId.inputText;
+		
+		var uuid = InputFlashUserUUID.inputText;
 		
 		if(uuid.length > 0){
 			console.log("Referrer UUID", uuid.length)
-			GetCustomerReferralsByUUID.run();
+			QueryReferee.run()
+			QueryReferrer.run()
+			
 		}
-		if(flashId.length > 0){
-			console.log("Referrer FlashId", flashId.length)
-			GetCustomerReferralsByFlashId.run();
-		}
-		
 		return [];
 		
 	},
 	getReferralData () {
-		var uuid = InputCustomerId.inputText;
-		var flashId = InputFlashId.inputText;
-		
-		if(uuid.length > 0){
-			console.log("Referrer UUID", uuid.length)
-			return GetCustomerReferralsByUUID.data;
-		}
-		if(flashId.length > 0){
-			console.log("Referrer FlashId", flashId.length)
-			return GetCustomerReferralsByFlashId.data;
-		}
+		// // var uuid = InputCustomerId.inputText;
+		// var flashId = InputFlashUserUUID.inputText;
+		// 
+		// if(uuid.length > 0){
+			// console.log("Referrer UUID", uuid.length)
+			// return GetCustomerReferralsByUUID.data;
+		// }
+		// if(flashId.length > 0){
+			// console.log("Referrer FlashId", flashId.length)
+			// return GetCustomerReferralsByFlashId.data;
+		// }
 		
 		return [];
 		
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
 	}
 }
